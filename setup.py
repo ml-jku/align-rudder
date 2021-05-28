26cf4b62a713e180063cefc2921981484ebb9165
Markus Hofmarcher <hofmarcher@ml.jku.at>
2021-05-28 10:46:52 +0200

    removed unnecessary tensorflow dependency

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='align_rudder',
       version='0.0.1',
-      install_requires=['gym', 'pandas', 'natsort', 'tqdm', 'tensorflow', 'biopython', 'scikit-learn',
+      install_requires=['gym', 'pandas', 'natsort', 'tqdm', 'biopython', 'scikit-learn',
                         'seaborn'],
       package_data={
           "align_rudder": ["demonstrations/**/*.npy", "demonstrations/**/*.p", "envs/policies/*.npy"]
